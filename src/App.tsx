39e8a102bd57752f6695959b260d8380d411fc56
ngxh <hgxn1101@gmail.com>
2022-09-14 15:56:46 +0800

    🦄 refactor: recoil -> useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { Outlet } from 'react-router-dom'
-import { RecoilRoot } from 'recoil'
-
 
 function App() {
   return (
     <div className="w-full h-full">
-      <RecoilRoot>
-        <Outlet />
-      </RecoilRoot>
+      <Outlet />
     </div>
   )
 }

2ec4dd913d53053c4355cb8f4757cd480202156f
ngxh <hgxn1101@gmail.com>
2023-03-01 16:23:34 +0800

    ✨ feat: css

--- pages/n/[...slug].tsx
+++ pages/n/[...slug].tsx
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-import { PropsWithChildren, useRef, useState } from 'react'
+import { PropsWithChildren, useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 
 import type { GetStaticPaths, GetStaticProps } from 'next'
@@ -17,18 +17,30 @@ type IProps = PropsWithChildren<{
   layout: 'default' | 'refs' | 'ppt'
 }>
 
-const DefaultLayoutWrapper = (props: PropsWithChildren) => (
-  <div className="layout-default px-[15%]">
+const DefaultLayoutWrapper = (props: PropsWithChildren<{ onClick: () => void }>) => (
+  <div className="layout-default px-[15%]" {...props}>
     <div className="px-[5%] bg-red-100 mesh">{props.children}</div>
   </div>
 )
 
-const RefsLayoutWrapper = (props: PropsWithChildren) => <div className="layout-refs">{props.children}</div>
+const RefsLayoutWrapper = (props: PropsWithChildren<{ onClick: () => void }>) => (
+  <div {...props} className="layout-refs">
+    {props.children}
+  </div>
+)
 
 const LayoutMap = {
   default: DefaultLayoutWrapper,
   refs: RefsLayoutWrapper,
   ppt: DefaultLayoutWrapper,
+}
+
+function Section(props: PropsWithChildren<{ className?: string; id?: string }>) {
+  return (
+    <section className={props.className} id={props.id}>
+      {props.children}
+    </section>
+  )
 }
 
 export default function Layout(props: IProps) {
@@ -37,31 +49,18 @@ export default function Layout(props: IProps) {
   const ref = useRef<HTMLDivElement>(null)
   const LayoutWrapper = LayoutMap[headerProps.layout]
 
-  const [maskvisible, setMaskVisible] = useState(false)
-
   const showToc = () => {
     if (!ref.current) return
-    // ref.current?.dataset?.toc === 'show'
-    // ? addDataset(ref.current!, { toc: 'hide' })
     addDataset(ref.current!, { toc: 'show' })
-    setMaskVisible(true)
   }
 
   return (
-    <div className={`mdx-render lg:px-10 xl:px-20 <sm:px-2 pb-5 relative text-primary`} ref={ref}>
+    <div className={`mdx-render lg:px-10 xl:px-20 <sm:px-2 pb-5 relative text-primary`} data-toc="hide" ref={ref}>
       <Header {...headerProps} changeToc={showToc} />
-      <LayoutWrapper>
-        <MDXComponent code={props.code} />
+      <LayoutWrapper onClick={() => addDataset(ref.current!, { toc: 'hide' })}>
+        <MDXComponent code={props.code} components={{ Section }} />
       </LayoutWrapper>
       <footer id="footer" className="absolute bottom-[3vh] left-[10vw] right-[10vw]  border-b-3 "></footer>
-      <div
-        className="mask fixed top-0 bottom-0 left-0 right-0  z-2 backdrop-filter backdrop-blur-2 bg-gray-200 bg-opacity-5"
-        style={{ display: maskvisible ? 'block' : 'none' }}
-        onClick={() => {
-          addDataset(ref.current!, { toc: 'hide' })
-          setMaskVisible(false)
-        }}
-      />
     </div>
   )
 }
@@ -98,9 +97,9 @@ export const getStaticProps: GetStaticProps = async (context) => {
       const isHeader = (str: string) => str.match(/^(#*?)\s/)?.[1].length || 0
       if (item.every((i) => i === '' || i === '\n')) return ''
       if (isHeader(item[0]) === 1) {
-        return `<section id="section-h1">\n${item.join('')}\n</section>\n`
+        return `<Section className="section" id="section-h1">\n${item.join('')}\n</Section>\n`
       }
-      return `<section>\n${item.join('')}\n</section>\n`
+      return `<Section className="section">\n${item.join('')}\n</Section>\n`
     })
     .join('')
 
